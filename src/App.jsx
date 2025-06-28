04573a8260fb0be4eab9354cc57a739360bc89b3
Ryan G <inquireryan@gmail.com>
2025-06-27 19:43:00 -0500

    Initial setup with Vite and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">Hello, Idea Board!</h1>
+    </div>
+  )
+}
+
+export default App
